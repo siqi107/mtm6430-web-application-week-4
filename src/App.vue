0b990ac21813c11189ef250ce36d9d0bdb2ba62b
Siqi Guo <guo00090@algonquinlive.com>
2019-06-14 10:49:43 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <input type="text">
+    </div>
+    <el-button type="primary" @click="openDiag = true">Welcome</el-button>
+    <el-dialog :visible.sync="openDiag">
+      <input type="text" v-model="sendTreat">
+      <HelloWorld msg="My first App"/>
+      <Simba v-bind:treat="sendTreat"/>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Simba from "./components/Simba";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Simba
+  },
+  data: function() {
+    return {
+      sendTreat: "Raw Hide",
+      openDiag: false
+    };
   }
 };
 </script>
---
+++ src/components/Simba.vue
@@ -0,0 +1,41 @@
+// create a component in vue cli
+// 1. create simba.vue
+// 2. import simba.vue to app.vue
+// 3. add to the list of component
+// 4. use simba tag name in parent template
+
+// use props
+// props: property coming from parent
+// 1. sending the vaule: use an attribute "treat" on app.vue
+// 2. receiving: props["treat"] in simba.vue
+// 3. display: {{ treat }} in simba.vue
+
+<template>
+  <div>
+    <h2>Hello {{ name }}!</h2>
+    <ul>
+      <li>Age: {{ age }}</li>
+      <li>Color: {{ color }}</li>
+      <li>{{ treat }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["treat"],
+  data: function() {
+    return {
+      name: "Simba",
+      age: 6,
+      color: "black"
+    };
+  }
+};
+</script>
+
+<style>
+h2 {
+  color: coral;
+}
+</style>
